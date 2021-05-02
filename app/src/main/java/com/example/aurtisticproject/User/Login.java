d0042ea39027255a8ee6e57dff677201ee0d927e
Apoorv Dubey <d123apoorv@gmail.com>
2021-05-02 17:29:47 +0530

    Commited by apoorv-> Sign up Updated FB

--- app/src/main/java/com/example/aurtisticproject/User/Login.java
+++ app/src/main/java/com/example/aurtisticproject/User/Login.java
@@ -18,6 +18,15 @@ import android.widget.Toast;
 import com.example.aurtisticproject.R;
 import com.example.aurtisticproject.User.Signup;
 import com.example.aurtisticproject.User.UserProfile;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.internal.metrics.Tag;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -29,6 +38,7 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -39,9 +49,14 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Login extends AppCompatActivity {
 
-    Button CallSignUp, login_btn, G_signin , fb_signin;
+    Button CallSignUp, login_btn, G_signin ;
+
+
 
     ImageView image;
 
@@ -64,6 +79,31 @@ public class Login extends AppCompatActivity {
         }
     }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("TAG", "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Log.i("TAG", "onComplete: login completed with user: " + user);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });}
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,7 +118,13 @@ public class Login extends AppCompatActivity {
         password = findViewById(R.id.password);
         login_btn = findViewById(R.id.Login_btn);
         G_signin = findViewById(R.id.gsignin);
-        fb_signin = findViewById(R.id.fsignin);
+        LoginButton fbloginButton = findViewById(R.id.fsignin);
+
+        CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+        fbloginButton.setReadPermissions("public_profile","email", "user_birthday", "user_friends");
+
+
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -91,6 +137,55 @@ public class Login extends AppCompatActivity {
                 signIn();
             }
         });
+
+        //Facebook signin
+        fbloginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                //Sign in completed
+                Log.i("TAG", "onSuccess: logged in successfully");
+                handleFacebookAccessToken(loginResult.getAccessToken());
+
+                //Getting the user information
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        // Application code
+                        Log.i("TAG", "onCompleted: response: " + response.toString());
+                        try {
+                            String email = object.getString("email");
+                            String birthday = object.getString("birthday");
+
+                            Log.i("TAG", "onCompleted: Email: " + email);
+                            Log.i("TAG", "onCompleted: Birthday: " + birthday);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.i("TAG", "onCompleted: JSON exception");
+                        }
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("TAG", "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("TAG", "facebook:onError", error);
+            }
+        });
+
+
+
 
         CallSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
