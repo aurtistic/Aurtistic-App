9675134eb99667b86cef0c7e68a8e9eb24311a77
NamanJain6499 <na.jain660@gmail.com>
2021-05-05 21:00:48 +0530

    Added url

--- app/src/main/java/com/example/aurtisticproject/User/LookAroundYourself.java
+++ app/src/main/java/com/example/aurtisticproject/User/LookAroundYourself.java
@@ -27,45 +27,58 @@ public class LookAroundYourself extends AppCompatActivity {
         setContentView(R.layout.activity_look_around_yourself);
     }
 
-    public void call_andhra(View view){
-         startActivity(new Intent(this, AndhraPradesh.class));}
+    public void call_andhra(View view) {
+        startActivity(new Intent(this, AndhraPradesh.class));
+    }
 
-    public void call_arunachal(View view){
-        startActivity(new Intent(this, ArunachalPradesh.class));}
+    public void call_arunachal(View view) {
+        startActivity(new Intent(this, ArunachalPradesh.class));
+    }
 
-    public void call_assam(View view){
-        startActivity(new Intent(this, Assam.class));}
+    public void call_assam(View view) {
+        startActivity(new Intent(this, Assam.class));
+    }
 
-    public void call_bihar(View view){
-        startActivity(new Intent(this, Bihar.class));}
+    public void call_bihar(View view) {
+        startActivity(new Intent(this, Bihar.class));
+    }
 
-    public void call_chattisgarh(View view){
-        startActivity(new Intent(this, Chattisgarh.class));}
+    public void call_chattisgarh(View view) {
+        startActivity(new Intent(this, Chattisgarh.class));
+    }
 
-    public void call_goa(View view){
-        startActivity(new Intent(this, Goa.class));}
+    public void call_goa(View view) {
+        startActivity(new Intent(this, Goa.class));
+    }
 
-    public void call_gujarat(View view){
-        startActivity(new Intent(this, Gujarat.class));}
+    public void call_gujarat(View view) {
+        startActivity(new Intent(this, Gujarat.class));
+    }
 
-    public void call_haryana(View view){
-        startActivity(new Intent(this, Haryana.class));}
+    public void call_haryana(View view) {
+        startActivity(new Intent(this, Haryana.class));
+    }
 
-    public void call_himachal(View view){
-        startActivity(new Intent(this, HimachalPradesh.class));}
+    public void call_himachal(View view) {
+        startActivity(new Intent(this, HimachalPradesh.class));
+    }
 
-    public void call_jharkhand(View view){
-        startActivity(new Intent(this, Jharkhand.class));}
+    public void call_jharkhand(View view) {
+        startActivity(new Intent(this, Jharkhand.class));
+    }
 
-    public void openurl(String s){
+    public void openurl(String s) {
         Uri uri = Uri.parse(s);
-        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
     }
 
     public void call_karnataka(View view) {
+
         startActivity(new Intent(this, Karnataka.class));
     }
+
     public void call_kerala(View view) {
+
         startActivity(new Intent(this, Kerala.class));
     }
 
@@ -73,11 +86,13 @@ public class LookAroundYourself extends AppCompatActivity {
         startActivity(new Intent(this, MadhyaPradesh.class));
     }
 
-    public void call_maharashtra(View view) {
+    public void call_maharashtra(View view)
+    {
         startActivity(new Intent(this, Maharashtra.class));
     }
 
     public void call_manipur(View view) {
+
         startActivity(new Intent(this, Manipur.class));
     }
 
@@ -87,17 +102,23 @@ public class LookAroundYourself extends AppCompatActivity {
     }
 
     public void call_mizoram(View view) {
+
         startActivity(new Intent(this, Mizoram.class));
     }
+
     public void call_nagaland(View view) {
-        startActivity(new Intent(this,Nagaland.class));
+
+        startActivity(new Intent(this, Nagaland.class));
     }
+
     public void call_delhi(View view) {
-        startActivity(new Intent(this,Delhi.class));
+
+        startActivity(new Intent(this, Delhi.class));
     }
 
 
     public void call_odisha(View view) {
-        startActivity(new Intent(this,Odisha.class));
+
+        startActivity(new Intent(this, Odisha.class));
     }
 }
