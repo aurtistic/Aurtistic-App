9b574ba1f13881e55e7b17d7254f41998e4cb493
Ankit <ankitvatsav23@gmail.com>
2021-05-05 15:50:04 +0530

    Added till Gujarat

--- app/src/main/java/com/example/aurtisticproject/Gujarat.java
+++ app/src/main/java/com/example/aurtisticproject/Gujarat.java
@@ -2,7 +2,10 @@ package com.example.aurtisticproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Gujarat extends AppCompatActivity {
 
@@ -11,4 +14,46 @@ public class Gujarat extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gujarat);
     }
+
+    public void call_school_gujarat1(View view){
+        openurl("https://goo.gl/maps/jaEcS8Nw5pUxJQBMA");}
+
+    public void call_school_gujarat2(View view){
+        openurl("https://goo.gl/maps/Pr7u9hDd9QypreoE9");}
+
+    public void call_school_gujarat3(View view){
+        openurl("https://g.page/sairesidentialschool?share");}
+
+    public void call_school_gujarat4(View view){
+        openurl("https://goo.gl/maps/Er8bHtU626b7cq8JA");}
+
+    public void call_school_gujarat5(View view){
+        openurl("https://goo.gl/maps/LPtp57o7iTRED4bG8");}
+
+    public void call_school_gujarat6(View view){
+        openurl("https://goo.gl/maps/vzXLTyUjA9QqHhB76");}
+
+    public void call_school_gujarat7(View view){
+        openurl("https://g.page/pearlspecialneedsfoundation?share");}
+
+    public void call_school_gujarat8(View view){
+        openurl("https://goo.gl/maps/opStj3nhEHGxjY1t7");}
+
+    public void call_school_gujarat9(View view){
+        openurl("https://g.page/samvedna-learning-centre-for-spe?share");}
+
+    public void call_school_gujarat10(View view){
+        openurl("https://goo.gl/maps/TY1unDGn3ZBmnSnx9");}
+
+    public void call_school_gujarat11(View view){
+        openurl("https://goo.gl/maps/8FVroiYhJqTUjtp4A");}
+
+    public void call_school_gujarat12(View view){
+        openurl("https://goo.gl/maps/oWSqUJtaubk8o3Ye7");}
+
+
+
+    public void openurl(String s){
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));}
 }
--- app/src/main/java/com/example/aurtisticproject/Haryana.java
+++ app/src/main/java/com/example/aurtisticproject/Haryana.java
@@ -2,7 +2,10 @@ package com.example.aurtisticproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Haryana extends AppCompatActivity {
 
@@ -11,4 +14,39 @@ public class Haryana extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_haryana);
     }
+
+    public void call_school_haryana1(View view){
+        openurl("https://goo.gl/maps/zmDbAt2Rspe6qwMa6");}
+
+    public void call_school_haryana2(View view){
+        openurl("https://goo.gl/maps/75NbrX68CtS8xESD9");}
+
+    public void call_school_haryana3(View view){
+        openurl("https://g.page/urja-school-for-special-needs-ch?share");}
+
+    public void call_school_haryana4(View view){
+        openurl("https://goo.gl/maps/3vaczKEW8E4PojQC7");}
+
+    public void call_school_haryana5(View view){
+        openurl("https://goo.gl/maps/PGHmLQLRFkAGRZ8k8");}
+
+    public void call_school_haryana6(View view){
+        openurl("https://goo.gl/maps/v81s1AiM1iWnN6MYA");}
+
+    public void call_school_haryana7(View view){
+        openurl("https://goo.gl/maps/5XT8J27ZSRspJsHw6");}
+
+    public void call_school_haryana8(View view){
+        openurl("https://goo.gl/maps/8AvoRGREgkMRWK2R9");}
+
+    public void call_school_haryana9(View view){
+        openurl("https://goo.gl/maps/m9eqZ6zQ6h3z77rD8");}
+
+    public void call_school_haryana10(View view){
+        openurl("https://goo.gl/maps/tcSdHvhnAmiNPVDn9");}
+
+
+        public void openurl(String s){
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));}
 }
