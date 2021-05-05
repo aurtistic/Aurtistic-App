a07eb262997129d34d37a1ba7e25df75a468a9d8
Ankit <ankitvatsav23@gmail.com>
2021-05-05 16:08:18 +0530

    Updated till Jharkhand

--- app/src/main/java/com/example/aurtisticproject/HimachalPradesh.java
+++ app/src/main/java/com/example/aurtisticproject/HimachalPradesh.java
@@ -2,7 +2,10 @@ package com.example.aurtisticproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class HimachalPradesh extends AppCompatActivity {
 
@@ -11,4 +14,15 @@ public class HimachalPradesh extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_himachal_pradesh);
     }
+
+    public void call_school_hp1(View view){
+        openurl("https://goo.gl/maps/JQwMFhmCAgJoh2c37");}
+
+    public void call_school_hp2(View view){
+        openurl("https://goo.gl/maps/y1dgB5BLujhsL9Hi7");}
+
+
+    public void openurl(String s){
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));}
 }
--- app/src/main/java/com/example/aurtisticproject/Jharkhand.java
+++ app/src/main/java/com/example/aurtisticproject/Jharkhand.java
@@ -2,7 +2,10 @@ package com.example.aurtisticproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Jharkhand extends AppCompatActivity {
 
@@ -11,4 +14,27 @@ public class Jharkhand extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jharkhand);
     }
+
+    public void call_school_jharkhand1(View view){
+        openurl("https://goo.gl/maps/jkGeofij8u28dYW59");}
+
+    public void call_school_jharkhand2(View view){
+        openurl("https://goo.gl/maps/B97nFTDF9CZqQHuJA");}
+
+    public void call_school_jharkhand3(View view){
+        openurl("https://goo.gl/maps/cPe9cuL8roxhGXer8");}
+
+    public void call_school_jharkhand4(View view){
+        openurl("https://goo.gl/maps/3K5FmWXJnz8qXMVVA");}
+
+    public void call_school_jharkhand5(View view){
+        openurl("https://goo.gl/maps/dKuH3ihLs7wLaJwG6");}
+
+    public void call_school_jharkhand6(View view){
+        openurl("https://goo.gl/maps/2VKNepHEcsj9zTro6");}
+
+
+    public void openurl(String s){
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));}
 }
